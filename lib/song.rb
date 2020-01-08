8b8e6e2f34aac85a5325c92a961e3c0b96085af1
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-08 23:26:56 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,30 +1,44 @@
 class Song 
-  @@count = 0 
-  @@genres= []
-  def initialize (name,artist,genre)
-    @name = name 
-    @artist = artist 
-    @genre = genre 
-    @@count += 1
-    @@genres = []:genre 
-    @@artists = []:artist
-    @@genre_count = {"rap"=>[] ,"rock"=>[] , "country"=>[]}
-    @@artist_count = []
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @@count +=1
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@artists << artist
+    @@genres << genre
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+    library = {}
+    @@genres.each do |genre|
+      if library.key?(genre)
+        library[genre] += 1 
+      else
+        library[genre] = 1 
+      end
+    end
+    library
+  end
+  def self.artist_count 
+    inter = {}
+    @@artists.each do |artist|
+      if artist.key?(artist)
+        inter[artist] += 1 
+      else
+        inter[artist] = 1 
+      end 
     end 
-  attr_accessor :name , :artist , :genre 
-  def self.count 
-    @@count = count 
-  end 
-  def self.genres 
-    @@genres = genres 
-  end 
-  def self.artists 
-    @@artists = artists
-  end 
-  def self.genre_count 
-    @@genre_count = genre_count
-  end 
-  def self.artist_count
-    @@artist_count = artist_count
-  end 
+  
 end 
